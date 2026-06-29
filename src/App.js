01c96002ab32d97d4dbcf737a5dc4dd1d7bac4dd
Karla <karla.perezn96@alumnostrazos.net>
2026-06-29 18:52:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
