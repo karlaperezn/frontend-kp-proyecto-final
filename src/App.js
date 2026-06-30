765af0bd060d885bbac5b7e5e626ce70a24c0887
Karla <karla.perezn96@alumnostrazos.net>
2026-06-30 16:38:39 +0200

    prueba 3 comentar import en app y borrar css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import { useState, useEffect } from 'react';
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <p>Hola mundo</p>
-    </BrowserRouter>
+    <div>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
   );
 }
 
