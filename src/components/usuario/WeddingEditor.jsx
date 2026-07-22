d481329115c64b0544a7092031ca27ded49e205e
karlaperezn <karla.perezn96@alumnostrazos.net>
2026-07-22 12:36:00 +0200

    correccion vercel 3 WeddingEditor fetchingwedding

--- src/components/usuario/WeddingEditor.jsx
+++ src/components/usuario/WeddingEditor.jsx
@@ -37,7 +37,7 @@ export function WeddingEditor({ modo, weddingData, setWeddingData, selectedWeddi
             }
             fetchWedding()
         }
-    }, [modo, weddingId])
+    }, [modo, weddingId, setWeddingData])
 
     async function saveWedding() {
         if (modo === "create") {
